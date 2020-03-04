2279edba263e8d9e0ea67e43a36aa1fdaa26b352
Xuan <tanqazx@gmail.com>
2020-03-03 23:15:45 -0500

    Add setindex! method for states.

--- src/states.jl
+++ src/states.jl
@@ -1,26 +1,18 @@
 "Construct state from a list of terms (e.g. initial predicates and fluents)."
 function State(terms::Vector{Term})
-    facts = Set{Term}()
-    fluents = Dict{Symbol,Any}()
+    state = State(Set{Term}(), Dict{Symbol,Any}())
     for t in terms
         if t.name == :(==)
             # Initialize fluents
             term, val = t.args[1], t.args[2]
             @assert !isa(term, Var) "Initial terms cannot be unbound variables."
             @assert isa(val, Const) "Terms must be initialized to constants."
-            if isa(term, Const)
-                # Assign term to constant value
-                fluents[term.name] = val.name
-            else
-                # Assign entry in look-up table
-                lookup = get!(fluents, term.name, Dict())
-                lookup[Tuple(a.name for a in term.args)] = val.name
-            end
+            state[term] = val.name
         else
-            push!(facts, t)
+            push!(state.facts, t)
         end
     end
-    return State(facts, fluents)
+    return state
 end
 
 "Access the value of a fluent or fact in a state."
@@ -36,3 +28,16 @@ Base.getindex(state::State, domain::Domain, term::String) =
     evaluate(Parser.parse_formula(term), state, domain; as_const=false)
 Base.getindex(state::State, domain::Domain, term::Union{Number,Symbol,Expr}) =
     evaluate(eval(Julog.parse_term(term)), state, domain; as_const=false)
+
+"Set the value of a fluent or fact in a state."
+Base.setindex!(state::State, val::Bool, term::Const) =
+    (if val push!(state.facts, term) else delete!(state.facts, term) end; val)
+Base.setindex!(state::State, val::Bool, term::Compound) =
+    (if val push!(state.facts, term) else delete!(state.facts, term) end; val)
+Base.setindex!(state::State, val::Any, term::Const) =
+    (state.fluents[term.name] = val)
+Base.setindex!(state::State, val::Any, term::Compound) =
+    (d = get!(state.fluents, term.name, Dict());
+     d[Tuple(a.name for a in term.args)] = val)
+Base.setindex!(state::State, val::Any, term::Union{Number,Symbol,Expr}) =
+    Base.setindex!(state, val, eval(Julog.parse_term(term)))
