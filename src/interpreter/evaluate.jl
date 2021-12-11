b32dd60d55a8d8a0d433f1adb915ceeb334c1faa
Xuan <tanqazx@gmail.com>
2021-12-11 18:10:22 -0500

    Fix tests for domain constants.

--- src/interpreter/evaluate.jl
+++ src/interpreter/evaluate.jl
@@ -5,7 +5,7 @@ function evaluate(interpreter::Interpreter,
     # Return if formula evaluates to a Const
     if isa(val, Const) && (!isa(val.name, Symbol) || !is_pred(val, domain))
         return val.name
-    elseif is_pred(val, domain) # Satisfy if we evaluate to a predicate
+    elseif is_pred(val, domain) || is_type(val, domain) # Satisfy if we evaluate to a predicate
         return satisfy(interpreter, domain, state, val)
     elseif val in get_objects(state) # Return object constant
         return val
