44b2a434ece0c7b0d17f6526b0df5f448d57bc57
Xuan <tanqazx@gmail.com>
2022-01-19 17:44:15 -0500

    Fix compiled transition to work with no-ops.

--- src/compiler/transition.jl
+++ src/compiler/transition.jl
@@ -2,22 +2,22 @@ function generate_transition(domain::Domain, state::State,
                              domain_type::Symbol, state_type::Symbol)
     transition_def = quote
         function transition(domain::$domain_type, state::$state_type,
-                            term::Term)
-            execute(domain, state, get_action(domain, term.name), term.args)
-        end
-        function transition(domain::$domain_type, state::$state_type,
                             term::Term; check::Bool=false)
-            execute(domain, state, get_action(domain, term.name), term.args;
-                    check=check)
-        end
-        function transition!(domain::$domain_type, state::$state_type,
-                             term::Term)
-            execute!(domain, state, get_action(domain, term.name), term.args)
+            if term.name == get_name(PDDL.no_op) && isempty(term.args)
+                execute(domain, state, PDDL.no_op, term.args; check=check)
+            else
+                execute(domain, state, get_action(domain, term.name), term.args;
+                        check=check)
+            end
         end
         function transition!(domain::$domain_type, state::$state_type,
                              term::Term; check::Bool=false)
-            execute!(domain, state, get_action(domain, term.name), term.args;
-                     check=check)
+            if term.name == get_name(PDDL.no_op) && isempty(term.args)
+                execute(domain, state, PDDL.no_op, term.args; check=check)
+            else
+                execute(domain, state, get_action(domain, term.name), term.args;
+                        check=check)
+            end
         end
     end
     return transition_def
