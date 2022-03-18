1742ac53c050be1a86b28fe5b3a63a899e866437
Xuan <tanqazx@gmail.com>
2022-03-18 18:01:26 -0400

    Add warnings to docstrings, simply de/register! implementation

--- src/compiler/compiler.jl
+++ src/compiler/compiler.jl
@@ -25,8 +25,15 @@ include("execute.jl")
 
 Compile a `domain` and `state` and return the resulting compiled domain
 and compiled state. A `problem` maybe provided instead of a state.
-Note that this function must be called at the top-level in order to avoid
-world-age errors.
+
+!!! warning "Top-Level Only"
+    Because `compiled` defines new types and methods, it should only be
+    called at the top-level in order to avoid world-age errors.
+
+!!! warning "Precompilation Not Supported"
+    Because `compiled` evaluates code in the `PDDL` module, it will lead to
+    precompilation errors when used in another module or package. Modules
+    which call `compiled` should hence disable precompilation.
 """
 function compiled(domain::Domain, state::State)
     # Generate definitions
--- src/extensions.jl
+++ src/extensions.jl
@@ -3,109 +3,67 @@
 
 Register `x` as a global datatype, predicate, function or modifier under
 the specified `name`.
+
+!!! warning "Top-Level Only"
+    Because `register!` defines new methods, it should only be called at the
+    top-level in order to avoid world-age errors.
+
+!!! warning "Precompilation Not Supported"
+    Because `register!` evaluates code in the `PDDL` module, it will lead to
+    precompilation errors when used in another module. For this reason, the
+    `@register` macro is preferred, and this function should only be used in
+    scripting contexts.
 """
 function register!(category::Symbol, name::Symbol, x)
     if category == :datatype
-        register_datatype!(name, x)
+        fname = GlobalRef(@__MODULE__, :datatype_def)
     elseif category == :predicate
-        register_predicate!(name, x)
+        fname = GlobalRef(@__MODULE__, :predicate_def)
     elseif category == :function
-        register_function!(name, x)
+        fname = GlobalRef(@__MODULE__, :function_def)
     elseif category == :modifier
-        register_modifier!(name, x)
+        fname = GlobalRef(@__MODULE__, :modifier_def)
     else
         error("Category must be :datatype, :predicate, :function or :modifier.")
     end
+    @eval $fname(::Val{$(QuoteNode(name))}) = $(QuoteNode(x))
 end
 
 register!(category::Symbol, name::AbstractString, x) =
     register!(category, Symbol(name), x)
 
-"Register global datatype."
-function register_datatype!(name::Symbol, ty)
-    _datatype_def = GlobalRef(@__MODULE__, :datatype_def)
-    @eval $_datatype_def(::Val{$(QuoteNode(name))}) = $(QuoteNode(ty))
-end
-
-"Register global predicate."
-function register_predicate!(name::Symbol, f)
-    _predicate_def = GlobalRef(@__MODULE__, :predicate_def)
-    @eval $_predicate_def(::Val{$(QuoteNode(name))}) = $(QuoteNode(f))
-end
-
-"Register global function."
-function register_function!(name::Symbol, f)
-    _function_def = GlobalRef(@__MODULE__, :function_def)
-    @eval $_function_def(::Val{$(QuoteNode(name))}) = $(QuoteNode(f))
-end
-
-"Register global modifier."
-function register_modifier!(name::Symbol, f)
-    _modifier_def = GlobalRef(@__MODULE__, :modifier_def)
-    @eval $_modifier_def(::Val{$(QuoteNode(name))}) = $(QuoteNode(f))
-end
-
 """
     deregister!([:datatype|:predicate|:function|:modifier], name)
 
 Deregister the datatype, predicate, function or modifier specified by `name`.
+
+!!! warning "Top-Level Only"
+    Because `deregister!` deletes existing methods, it should only be called
+    at the top-level in order to avoid world-age errors. It should primarily
+    be used in scripting contexts, and not by other packages or modules.
 """
 function deregister!(category::Symbol, name::Symbol)
     if category == :datatype
-        deregister_datatype!(name)
+        f = datatype_def
     elseif category == :predicate
-        deregister_predicate!(name)
+        f = predicate_def
     elseif category == :function
-        deregister_function!(name)
+        f = function_def
     elseif category == :modifier
-        deregister_modifier!(name)
+        f = modifier_def
     else
         error("Category must be :datatype, :predicate, :function or :modifier.")
+    end
+    if hasmethod(f, Tuple{Val{name}})
+        m = first(methods(f, Tuple{Val{name}}))
+        Base.delete_method(m)
+    else
+        @warn "No registered $f :$name to deregister."
     end
 end
 
 deregister!(category::Symbol, name::AbstractString) =
     deregister!(category, Symbol(name))
-
-"Deregister global datatype."
-function deregister_datatype!(name::Symbol)
-    if hasmethod(datatype_def, Tuple{Val{name}})
-        m = first(methods(datatype_def, Tuple{Val{name}}))
-        Base.delete_method(m)
-    else
-        @warn "No registered datatype :$name to deregister."
-    end
-end
-
-"Deregister global predicate."
-function deregister_predicate!(name::Symbol)
-    if hasmethod(predicate_def, Tuple{Val{name}})
-        m = first(methods(predicate_def, Tuple{Val{name}}))
-        Base.delete_method(m)
-    else
-        @warn "No registered predicate :$name to deregister."
-    end
-end
-
-"Deregister global function."
-function deregister_function!(name::Symbol)
-    if hasmethod(function_def, Tuple{Val{name}})
-        m = first(methods(function_def, Tuple{Val{name}}))
-        Base.delete_method(m)
-    else
-        @warn "No registered predicate :$name to deregister."
-    end
-end
-
-"Deregister global modifier."
-function deregister_modifier!(name::Symbol)
-    if hasmethod(modifier_def, Tuple{Val{name}})
-        m = first(methods(modifier_def, Tuple{Val{name}}))
-        Base.delete_method(m)
-    else
-        @warn "No registered predicate :$name to deregister."
-    end
-end
 
 """
     attach!(domain, :function, name, f)
