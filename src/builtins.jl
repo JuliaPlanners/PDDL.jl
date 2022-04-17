cb10a7558adf48e6f07506db240b7ff0982de00f
Xuan <tanqazx@gmail.com>
2022-04-16 23:05:17 -0400

    Avoid constructing dictionary when looking up global built-ins.

--- src/builtins.jl
+++ src/builtins.jl
@@ -14,22 +14,37 @@ Non-equivalence in concrete and abstract domains. Defaults to `a != b`.
 """
 nequiv(a, b) = a != b
 
+"Helper function that generates dictionary at compile time."
+@generated function _generate_dict(keys::Val{Ks}, values::Val{Vs}) where {Ks, Vs}
+    Vs = Vs isa Tuple ? Vs : Tuple(Vs.parameters) # Handle tuple types
+    dict = Dict(zip(Ks, Vs))
+    return :($dict)
+end
+
 "Mapping from PDDL data types to Julia types and default values."
 @valsplit datatype_def(Val(name::Symbol)) =
     error("Unknown datatype: $name")
 datatype_def(::Val{:boolean}) = (type=Bool, default=false)
 datatype_def(::Val{:integer}) = (type=Int, default=0)
-datatype_def(::Val{:numeric}) = (type=Float64, default=0.0)
+datatype_def(::Val{:numeric}) = (type=Float64, default=1.0)
+
+"Mapping from PDDL datatype to Julia type."
+datatype_typedef(name::Symbol) = datatype_def(name).type
+"Mapping from PDDL datatype to default value."
+datatype_default(name::Symbol) = datatype_def(name).default
 
 "Return list of global datatypes."
 global_datatype_names() =
     valarg_params(datatype_def, Tuple{Val}, Val(1), Symbol)
-"Return dictionary mapping global datatype names to implementations."
-global_datatypes() =
-    Dict{Symbol,Any}(d => datatype_def(Val(d)) for d in global_datatype_names())
 "Return whether a symbol refers to global datatype."
 is_global_datatype(name::Symbol) =
     valarg_has_param(name, datatype_def, Tuple{Val}, Val(1), Symbol)
+"Return dictionary mapping global datatype names to implementations."
+function global_datatypes()
+    names = global_datatype_names()
+    types = Base.to_tuple_type(getindex.(datatype_def.(names), 1))
+    return _generate_dict(Val(names), Val(types))
+end
 
 "Mapping from PDDL built-in predicates to Julia functions."
 @valsplit predicate_def(Val(name::Symbol)) =
@@ -43,12 +58,15 @@ predicate_def(::Val{:>}) = >
 "Return list of all global predicate names."
 global_predicate_names() =
     valarg_params(predicate_def, Tuple{Val}, Val(1), Symbol)
-"Return dictionary mapping global predicate names to implementations."
-global_predicates() =
-    Dict{Symbol,Any}(p => predicate_def(p) for p in global_predicate_names())
 "Return whether a symbol refers to global predicate."
 is_global_pred(name::Symbol) =
     valarg_has_param(name, predicate_def, Tuple{Val}, Val(1), Symbol)
+"Return dictionary mapping global predicate names to implementations."
+function global_predicates()
+    names = global_predicate_names()
+    defs = predicate_def.(names)
+    return _generate_dict(Val(names), Val(defs))
+end
 
 "Mapping from PDDL built-in functions to Julia functions."
 @valsplit function_def(Val(name::Symbol)) =
@@ -62,13 +80,16 @@ function_def(::Val{:/}) = /
 global_function_names() =
     (valarg_params(function_def, Tuple{Val}, Val(1), Symbol)...,
      valarg_params(predicate_def, Tuple{Val}, Val(1), Symbol)...)
-"Return dictionary mapping global function names to implementations."
-global_functions() =
-    Dict{Symbol,Any}(f => function_def(f) for f in global_function_names())
 "Return whether a symbol refers to global function."
 is_global_func(name::Symbol) =
     valarg_has_param(name, function_def, Tuple{Val}, Val(1), Symbol) ||
     is_global_pred(name)
+"Return dictionary mapping global function names to implementations."
+function global_functions()
+    names = global_function_names()
+    defs = function_def.(names)
+    return _generate_dict(Val(names), Val(defs))
+end
 
 "Mapping from PDDL modifiers (i.e. in-place assignments) to PDDL functions."
 @valsplit modifier_def(Val(name::Symbol)) =
@@ -80,11 +101,13 @@ modifier_def(::Val{Symbol("scale-down")}) = :/
 
 "Return list of all global modifier names."
 global_modifier_names() =
-    (valarg_params(modifier_def, Tuple{Val}, Val(1), Symbol)...,
-     valarg_params(predicate_def, Tuple{Val}, Val(1), Symbol)...)
-"Return dictionary mapping global modifier names to implementations."
-global_modifiers() =
-    Dict{Symbol,Any}(m => modifier_def(m) for m in global_modifier_names())
+    valarg_params(modifier_def, Tuple{Val}, Val(1), Symbol)
 "Return whether a symbol refers to global modifier."
 is_global_modifier(name::Symbol) =
     valarg_has_param(name, modifier_def, Tuple{Val}, Val(1), Symbol)
+"Return dictionary mapping global modifier names to implementations."
+function global_modifiers()
+    names = global_modifier_names()
+    defs = modifier_def.(names)
+    return _generate_dict(Val(names), Val(defs))
+end
