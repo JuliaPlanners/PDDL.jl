903f2c3ce6d6c99b8081275b6ce264b40570d32e
Xuan <tanqazx@gmail.com>
2022-12-18 19:13:39 -0500

    Use custom Documenter.jl branch for meta tags.

--- docs/make.jl
+++ docs/make.jl
@@ -1,30 +1,11 @@
 using Documenter, PDDL
 
-# Include workaround to inject custom meta tags
-include("metatags.jl")
-
-# Add custom metatags
-empty!(CUSTOM_META_TAGS)
-PREVIEW_IMAGE_URL =
-   "https://juliaplanners.github.io/PDDL.jl/dev/assets/preview-image.png"
-IMAGE_SUFFIX = Int(rand(UInt32))
-SITE_DESCRIPTION = "Documentation for the PDDL.jl automated planning library."
-append!(CUSTOM_META_TAGS, [
-   meta[:property => "description", :content => SITE_DESCRIPTION],
-   # OpenGraph tags
-   meta[:property => "og:type", :content => "website"],
-   meta[:property => "og:image", :content => PREVIEW_IMAGE_URL],
-   meta[:property => "og:description", :content => SITE_DESCRIPTION],
-   # Twitter tags
-   meta[:property => "twitter:card", :content => "summary_large_image"],
-   meta[:property => "twitter:image", :content => "$PREVIEW_IMAGE_URL?$IMAGE_SUFFIX"],
-   meta[:property => "twitter:description", :content => SITE_DESCRIPTION],
-])
-
 makedocs(
    sitename="PDDL.jl",
    format=Documenter.HTML(
       prettyurls=get(ENV, "CI", nothing) == "true",
+      canonical="https://juliaplanners.github.io/PDDL.jl/dev",
+      description="Documentation for the PDDL.jl automated planning library.",
       highlights=["lisp"] # Use Lisp highlighting as PDDL substitute
    ),
    pages=[
